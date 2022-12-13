12b104b2b1ba79aa0a21e35ab2cd1576558bd963
Luiz Galvão <85366930+luizloro@users.noreply.github.com>
2022-12-13 16:21:45 -0300

    Adicição do timer em js

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ const phoneMask = (value) => {
     value = value.replace(/(\d)(\d{4})$/, "$1-$2")
     return value
 }
+
+/* Try Catch repósitorio img unsplash */
+
+/* Timer do Header*/
+const timeLeft = document.querySelector('#timer')
+const nextYear = new Date().getFullYear() + 1
+const newYearTimer = new Date(`January 01 ${nextYear} 00:00:00`)
+
+const updateCoutDown = () => {
+    const currentTime = new Date()
+    const difference = newYearTimer - currentTime
+    const days = Math.floor(difference / 1000 / 60 /60 / 24)
+    const hours = Math.floor(difference / 1000 /60 /60) % 24
+    const minutes = Math.floor(difference / 1000 /60) % 60
+    const seconds = Math.floor(difference / 1000) % 60
+
+    timeLeft.textContent = seconds < 10 ? '0' + seconds : seconds
+
+    timeLeft.textContent = `Expira em: ${days} Dias ${hours} Horas : ${minutes} : Minutos : ${seconds} Segundos`
+
+
+}
+
+setInterval(updateCoutDown, 1000)
